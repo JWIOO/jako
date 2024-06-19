85a7281f6b7970a0823801875ec3aa492c1f6e26
Samuel.Park <samuel@floatic.io>
2024-06-19 23:49:01 +0900

    chore : delete kotlin

--- springboot-developer/src/main/kotlin/me/jiwoosong/springbootdeveloper/repository/BlogRepository.kt
+++
@@ -1,6 +0,0 @@
-package me.jiwoosong.springbootdeveloper.repository
-
-import me.jiwoosong.springbootdeveloper.domain.Article
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface BlogRepository : JpaRepository<Article?, Long?>
